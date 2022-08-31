552e8c0a2680ba59d2ad75af181cf227e3508419
sean.ronan <seanjoshuaronan@gmail.com>
2022-08-31 18:37:35 +0800

    fix: Initialize upon create

--- lib/managers/hive_manager.dart
+++ lib/managers/hive_manager.dart
@@ -4,6 +4,10 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pokemon/data/models/pokemon_model.dart';
 
 class HiveManager {
+  HiveManager() {
+    initialize();
+  }
+
   final Completer<void> _initCompleter = Completer<void>();
 
   Future<void> initialize() async {
